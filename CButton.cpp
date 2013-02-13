d23cd548afea0cb8c2ff416763f06a90be0ba5fc
Kirill <asapcss@gmail.com>
2013-02-13 10:54:51 -0500

    CButton

---
+++ CButton.cpp
@@ -0,0 +1,23 @@
+CButton::CButton(const char *Str, int Row, int Col, bool Bordered = true, const char* Border=C_BORDER_CHARS){
+	return 0;
+}
+
+virtual CButton::~CButton(){
+	return 0;
+}
+
+void CButton::draw(int rn=C_FULL_FRAME){
+	return 0;
+}
+
+int CButton::edit(){
+	return 0;
+}
+
+bool CButton::editable()const{
+	return 0;
+}
+
+void CButton::set(const void* str){
+	return 0;
+}
